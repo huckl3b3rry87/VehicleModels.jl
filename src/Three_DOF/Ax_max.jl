c0895dd6b2952bed7e23990adbe42af24118efd1
Huckleberry Febbo <febbo@umich.edu>
2016-10-26 16:08:10 -0400

    adjusted macros for Ax

--- src/Three_DOF/Ax_max.jl
+++ src/Three_DOF/Ax_max.jl
@@ -1,8 +1,8 @@
 @def Ax_max begin #(U)
 
 	# maximum longitudinal acceleration for given speed
-	Ax_max = Array(Float64,(N+1,1))
-	for i in 1:N+1
+	Ax_max = Array(Float64,(length(U),1))
+	for i in eachindex(U)
 		Ax_max[i,1] = AXC[1]*U[i]^3 + AXC[2]*U[i]^2 + AXC[3]*U[i] + AXC[4]
 	end
 	Ax_max
