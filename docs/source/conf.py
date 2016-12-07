e9288d1db1b58e7960954850ddc531a71c53d2c9
Huckleberry Febbo <febbo@umich.edu>
2016-12-07 15:09:20 -0500

    removed #    'sphinx.ext.githubpages',

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,8 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
+#    'sphinx.ext.githubpages',
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
