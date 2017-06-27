bdf1dbaecdb1df5c53aa471fcd19c55d07a77f9d
Huckleberry Febbo <febbo@umich.edu>
2017-06-26 21:36:47 -0400

    added updates for 0.6

--- src/Three_DOF/FZ_RR.jl
+++ src/Three_DOF/FZ_RR.jl
@@ -3,7 +3,7 @@ macro FZ_RR()#(N, F_yf, F_yr, V, Ax, r)
 		# define the lateral tire forces
 		F_yf=@F_YF()
 		F_yr=@F_YR()
-		FZ_rr = zeros(Float64, (length(Ax),1))
+		FZ_rr = zeros{Float64}((length(Ax),1))
 		for ii in eachindex(Ax)
 			FZ_rr[ii] = 0.5*(FzR0 + KZX*(Ax[ii] - V[ii]*R[ii])) + KZYR*((F_yf[ii] + F_yr[ii])/m)
 		end
